8c6c49047dec8956da925160ea95cfb5b9ebda3e
Alexander Irizarry <141542160+supernova147@users.noreply.github.com>
2025-07-20 10:48:10 -0400

    [UPDATE]--- CSS Editted, buttons added, images added; basic layout of page complete. Further additions including adding logic & interactivity w/ JS.

---
+++ script.js
@@ -0,0 +1,17 @@
+/*Logic Tree
+- Wait for user input & capture user input
+--Compare User input to computer's
+--if its equal.. add a draw.
+
+---if it isnt equal & its scissors > paper.. add W
+---if it isnt equal & its paper > rock.. add W
+---if it isnt equal & its rock > scissors.. add W
+
+---if it isnt equal & its paper < scissors.. add L
+---if it isnt equal & its rock < paper.. add L
+---if it isnt equal & its scissors < rock.. add L
+
+-Reset button is separate.
+---
+
+*/
