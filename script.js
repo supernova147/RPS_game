702fd90019214a0d2974d54536f96d14fc3069b3
Alexander Irizarry <141542160+supernova147@users.noreply.github.com>
2025-07-20 13:34:57 -0400

    [CHECKPOINT]--- Logic added to rock button; working on other features such as RESET & changing images for the user & comp.

--- script.js
+++ script.js
@@ -15,3 +15,47 @@
 ---
 
 */
+
+let wins_count = 0;
+let loss_count = 0;
+let draw_count = 0;
+
+const reset_count = 0;
+let user_choice;
+
+const choices = ['rock', 'paper', 'scissors'];
+let rng = Math.floor(Math.random() * 3);
+let comp_choice = choices[rng];
+
+console.log(comp_choice)
+function rock_function() {
+
+const choices = ['rock', 'paper', 'scissors'];
+let rng = Math.floor(Math.random() * 3);
+let comp_choice = choices[rng];
+
+console.log(comp_choice)
+
+    if(comp_choice === 'rock') {
+        draw_count += 1;
+        document.getElementById("draw_count").innerText = draw_count;
+        document.getElementById(comp_img).inner
+    }
+    else if(comp_choice === 'paper') {
+        loss_count += 1;
+        document.getElementById("loss_count").innerText = loss_count;
+
+    }
+    else {
+        wins_count += 1;
+        document.getElementById("wins_count").innerText = wins_count;
+    }
+}
+
+// paper_function {
+
+// }
+
+// scissors_function {
+
+// }
